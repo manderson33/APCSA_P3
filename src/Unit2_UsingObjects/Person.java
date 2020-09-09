d6fab544ff7b15ffd6ccb2815b7d1320932e8cbf
maanderson <maanderson@localhost>
2020-09-09 08:42:01 +0700

    Merge origin/master
    Conflicts:
    	build.xml
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

--- src/Unit2_UsingObjects/Person.java
+++ src/Unit2_UsingObjects/Person.java
@@ -10,59 +10,61 @@ package Unit2_UsingObjects;
  * @author maanderson
  */
 public class Person {
-    
+
     String name;
     int age;
     String gender;
     String city;
-    
-    public Person(String n, int age, String gender, String city){       
+
+    public Person(String n, int age, String gender, String city) {
         name = n;
         this.age = age;
         this.gender = gender;
         this.city = city;
-              
+
     }
-    
-    public Person(String n, int age, String gender){        
+
+    public Person(String n, int age, String gender) {
         name = n;
         this.age = age;
         this.gender = gender;
         this.city = "Jakarta";
-               
+
     }
-    
-    public Person(String name, int age){
+
+    public Person(String name, int age) {
         this.name = name;
         this.age = age;
         this.gender = "Female";
         this.city = "Jakarta";
     }
-    
-    public Person(){
+
+    public Person() {
         String name = "Juan";
     }
-    
-    
-    
+
+    @Override
+    public String toString() {
+        return "Name: " + name + " Age: " + age + " Gender: " + gender + " City: " + city;
+    }
+
 }
 
+class testPerson {
 
-class testPerson{
     public static void main(String[] args) {
+
+        Person student1 = new Person("Jennifer", 16, "Female");
+
+        Person student2 = new Person("Ian", 15, "Male", "Jakarta");
+
+        Person teacher = new Person("Ms. Tellez", 35, "Female", "Banten");
+
+        System.out.println(student1);
         
-       Person student2 = new Person("Jennifer", 16, "Female");
-        
-       Person student = new Person("Ian", 15, "Male", "Jakarta");
-        
-       Person teacher = new Person("Ms. Tellez", 35, "Female", "Banten");
-       
-       Person p = new Person();
-       
-       String word = "Literal";
-       
-       Person p2 = student2;
-       
-       
+        System.out.println(student2);
+
+        System.out.println(teacher);
+
     }
 }
